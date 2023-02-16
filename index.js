470c3c04502c7cdcf51a77ddbcde456e881f6342
Philippe Victoria <vp8@mbp-de-philippe.home>
2023-02-16 11:48:52 +0100

    ajout title/skip/limit

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const axios = require("axios");
 
 app.get("/comics", async (req, res) => {
   try {
+    const title = req.query.title || ";";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&title=${title}&skip=${skip}&limit=${limit}`
     );
     console.log(response.data);
     res.json(response.data);
@@ -19,11 +22,15 @@ app.get("/comics", async (req, res) => {
 
 app.get("/characters", async (req, res) => {
   try {
+    const name = req.query.name || "";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
+
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${name}&skip=${skip}&limit=${limit}`
     );
     console.log(response.data);
-    res.status(200).json(response.data.results);
+    res.status(200).json(response.data);
   } catch (error) {
     console.log(error.response);
   }
