b3127db57dcd15ed99fedf984a48f2c62ffed9f2
Philippe Victoria <vp8@mbp-de-philippe.home>
2023-04-10 12:49:27 +0200

    fix model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("User", {
   username: { type: String, unique: true, required: true },
-  password: String,
-  favorites: Array,
+  characterFavorites: Array,
+  comicFavorites: Array,
   token: String,
   hash: String,
   salt: String,
