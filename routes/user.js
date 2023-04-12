83498440bbf88b1ee4e6ad692d6223e0add61f8b
Philippe Victoria <vp8@mbp-de-philippe.home>
2023-04-12 14:59:46 +0200

    forget axios

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const uid2 = require("uid2");
